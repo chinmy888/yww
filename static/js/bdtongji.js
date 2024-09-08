a68dc955dd50880c7d0a9be42041507c6bffa68e
chinmy888 <411191866@qq.com>
2024-09-08 17:46:42 +0800

    Add files via upload

---
+++ static/js/bdtongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?39f7c9431fdd7a3d6e06a177938de82a";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
